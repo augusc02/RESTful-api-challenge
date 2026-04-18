88055c03a75b6380195ef99f6bdfd6e5e1649aab
augusc02 <augusc02@gmail.com>
2026-04-18 16:36:11 -0300

    Add default maven files and spring boot tests context file

---
+++ src/test/java/com/challenge/api/RestfulApiChallengeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.challenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
